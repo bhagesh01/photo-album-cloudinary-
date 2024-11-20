1d7e096e1a953aa168577fc3f399d0774f2aae60
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-11-20 22:20:31 +0530

    first commit for re installing

--- src/app/gallery/upload-button.tsx
+++ src/app/gallery/upload-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CldUploadButton } from "next-cloudinary";
-import { UploadResult } from "../page";
+// import { UploadResult } from "../page";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
